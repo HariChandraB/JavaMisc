010662b6bd464f857fc75b3055032c06a4eea0d5
HariChandraB <harihrps@gmail.com>
2021-02-11 03:19:01 -0600

    Create RemoveDuplicates.java
    initial push

---
+++ RemoveDuplicates.java
@@ -0,0 +1,18 @@
+Public class RemoveDuplicates{
+
+public static void main(String[] args){
+String string = "aabbccdefatafaz";
+
+char[] chars = string.toCharArray();
+Set<Character> charSet = new LinkedHashSet<Character>();
+for (char c : chars) {
+    charSet.add(c);
+}
+
+StringBuilder sb = new StringBuilder();
+for (Character character : charSet) {
+    sb.append(character);
+}
+System.out.println(sb.toString());
+}
+}
